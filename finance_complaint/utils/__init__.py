ff7b76f58d1c317a33a72b01d6f15a51e24ff1d5
Samarjeet Singh <20bcs6598@cuchd.in>
2023-02-14 01:08:28 +0530

    updated

--- finance_complaint/utils/__init__.py
+++ finance_complaint/utils/__init__.py
@@ -4,14 +4,18 @@ import yaml
 from finance_complaint.logger import logger
 from finance_complaint.exception import FinanceException
 
-def writre_yaml_file():
+def write_yaml_file(file_path:str, data:dict=None):
     try:
-        pass:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as yaml_file:
+            if data not not in None:
+                yaml.dump(data, yaml_file)
     except Exception as e:
         raise FinanceException(e,sys)
 
-def read_yaml_file():
+def read_yaml_file(file_path:str)->dict:
     try:
-        pass
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
     except Exception as e:
         raise FinanceException(e,sys)
